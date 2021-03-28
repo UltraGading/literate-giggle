17c1b8d7a3cb51e539b4bad0f0229e96ee15998b
UltraGading <73372173+UltraGading@users.noreply.github.com>
2021-03-28 13:56:25 -0700

    Add files via upload

---
+++ ms.py
@@ -0,0 +1,41 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics 
+import random
+import pandas as pd
+import csv
+df = pd.read_csv("ms.csv")
+data = df["Math_score"].tolist()
+df2 = pd.read_csv('ms2.csv')
+data2 = df['Math_score'].tolist()
+pop_meen = statistics.mean(data)
+print('pop mean', pop_meen)
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+mean_list = []
+for i in range(0,100):
+    set_of_means= random_set_of_mean(30)
+    mean_list.append(set_of_means)
+std_deviation = statistics.stdev(mean_list)
+mean = statistics.mean(mean_list)
+print("mean of sampling distribution:- ",mean)
+print("Standard deviation of sampling distribution:- ", std_deviation)
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+meen2 = statistics.mean(data2)
+fig = ff.create_distplot([mean_list], ["student marks"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[meen2, meen2], y=[0,0.17], mode='lines', name = 'NEW MEAN'))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+z_score = (meen2-mean)/std_deviation
+print(z_score)
